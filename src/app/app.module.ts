b2786f593ac4145cc62400ae83c09017fc8a5ffe
Tannus Esquerdo <tannus.esquerdo@dito.com.br>
2017-01-17 19:09:29 -0200

    Set up Pages to Model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { HomePage } from '../pages/home/home';
+import { ServicePage } from '../pages/service/service';
+import { SettingsPage } from '../pages/settings/settings';
+import { AboutPage } from '../pages/about/about';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    ServicePage,
+    SettingsPage,
+    AboutPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +20,10 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    ServicePage,
+    SettingsPage,
+    AboutPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
